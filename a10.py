05e68a235e7e3252c419ce2724a2d879b5a40e0a
1xfan <jomartinez2@cps.edu>
2026-05-22 10:48:12 -0500

    function 1

--- a10.py
+++ a10.py
@@ -153,6 +153,21 @@ def get_death_date(name: str) -> str:
 
     return match.group("death")
 
+def get_capital(country: str) -> str:
+    """Gets the capital of the given country
+
+    Args:
+        country - name of the country
+
+    Returns:
+        capital of the given country
+    """
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(country)))
+    pattern = r"Capital(?: city)?:?\s*(?P<capital>[A-Za-z .-]+)"
+    error_text = "Page infobox has no information about the capital of the country."
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("capital")#.strip()
+
 
 # below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
@@ -192,6 +207,16 @@ def polar_radius(matches: List[str]) -> List[str]:
     """
     return [get_polar_radius(matches[0])]
 
+def capital(matches: List[str]) -> List[str]:
+    """Gets the capital of the given country
+
+    Args:
+        country - name of the country
+
+    Returns:
+        capital of the given country
+    """
+    return [get_capital(" ".join(matches))]
 
 # dummy argument is ignored and doesn't matter
 def bye_action(dummy: List[str]) -> None:
@@ -209,6 +234,7 @@ pa_list: List[Tuple[Pattern, Action]] = [
     ("when was % born".split(), birth_date),
     ("when did % die".split(), death_date),
     ("what is the polar radius of %".split(), polar_radius),
+    ("what is the capital of %".split(), capital),
     (["bye"], bye_action),
 ]
 
